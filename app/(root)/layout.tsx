a74199f5812ef49fc0273d453e4e5c9770ce2e7b
mfukechristian <mfukechristian@gmail.com>
2025-02-24 17:56:19 +0200

    feat: add header and footer components, update root layout structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,23 @@
+import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col">
-      <main className="flex-1 wrapper">{children}</main>
-    </div>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <div className="flex h-screen flex-col">
+          <Header />
+          <main className="flex-1 wrapper">{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
